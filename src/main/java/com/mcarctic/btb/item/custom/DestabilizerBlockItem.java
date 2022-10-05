cbf0ec3ed10af634186d4d1a58870bc40dca6067
McArctic <creepergaming490@gmail.com>
2022-10-05 16:09:14 -0400

    Fixed Destabilizer Item

--- src/main/java/com/mcarctic/btb/item/custom/DestabilizerBlockItem.java
+++ src/main/java/com/mcarctic/btb/item/custom/DestabilizerBlockItem.java
@@ -7,12 +7,14 @@ import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.IItemRenderProperties;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
 import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
 import java.util.function.Consumer;
+
 
 public class DestabilizerBlockItem extends BlockItem implements IAnimatable {
     public AnimationFactory factory = new AnimationFactory(this);
@@ -22,8 +24,10 @@ public class DestabilizerBlockItem extends BlockItem implements IAnimatable {
     }
 
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
+        event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.destabilizer.idle", true));
         return PlayState.CONTINUE;
     }
+
 
     @Override
     public void initializeClient(Consumer<IItemRenderProperties> consumer) {
@@ -48,3 +52,5 @@ public class DestabilizerBlockItem extends BlockItem implements IAnimatable {
         return this.factory;
     }
 }
+
+
